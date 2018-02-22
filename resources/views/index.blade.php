cce27be87307d2b6e7e68d7fa942edacba18d44c
Askhat Abishev <asky.gm@gmail.com>
2018-02-22 21:50:19 +0600

    core: Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <table>
+        <tr>
+            <th>Название файла</th>
+            <th>Создан</th>
+            <th>Изменен</th>
+        </tr>
+    </table>
+@endsection
